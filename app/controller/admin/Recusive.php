de5af64c52c91c2e12179be0c5100a784a5e690f
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-05-12 16:42:56 +0700

    commit a

--- app/controller/admin/Recusive.php
+++ app/controller/admin/Recusive.php
@@ -23,7 +23,7 @@ class Recusive
                         $this->recusion .= "<option value='" . $value['ma_loai_sp'] . "'>" . $text . $value['ten_loai'] . "</option >";
                     }
                 }
-                $this->categories($prend_id, $value['ma_loai_sp'], $text . '-');
+                // $this->categories($prend_id, $value['ma_loai_sp'], $text . '-');
             }
         }
         return $this->recusion;
